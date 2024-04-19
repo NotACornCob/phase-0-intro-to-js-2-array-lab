110eed05a69a06338967006a715e36a457f975c7
Alex <alex.kleinman@gmail.com>
2024-04-19 11:29:54 -0400

    bug free lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = [
+    "Milo", 
+    "Otis", 
+    "Garfield"
+];
+
+function destructivelyAppendCat(name) {
+    cats.push(name); 
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name); 
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+
+function appendCat(name) {
+    const newArray = [...cats, name];
+    return newArray;
+}
+
+function prependCat(name) {
+    const newArray = [name, ...cats];
+    return newArray;
+}
+
+function removeLastCat() {
+    const catRemoval = cats.slice(0,2);
+    return catRemoval;
+}
+
+function removeFirstCat() {
+    const catRemoval = cats.slice(1);
+    return catRemoval;
+}
